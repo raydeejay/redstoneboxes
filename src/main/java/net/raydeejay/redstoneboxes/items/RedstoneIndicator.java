56fceeacd769c693be8612712be0c7f4b04e64b3
Sergi Reyner <sergi.reyner@gmail.com>
2018-03-06 20:39:44 +0000

    Some files got left behind

--- src/main/java/net/raydeejay/redstoneboxes/items/RedstoneIndicator.java
+++ src/main/java/net/raydeejay/redstoneboxes/items/RedstoneIndicator.java
@@ -1,4 +1,26 @@
 package net.raydeejay.redstoneboxes.items;
 
-public class RedstoneIndicator {
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.raydeejay.redstoneboxes.RedstoneBoxesMod;
+
+public class RedstoneIndicator extends Item {
+    private final String name = "redstone_indicator";
+
+    public RedstoneIndicator() {
+        setUnlocalizedName(RedstoneBoxesMod.MODID + "." + name);
+        setCreativeTab(CreativeTabs.MISC);
+        setRegistryName(new ResourceLocation(RedstoneBoxesMod.MODID, name));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0,
+                                                   new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
 }
